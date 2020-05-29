fb7bd10034343e730701f0b77e8d2ab6280ed23e
Hosomi.Fuminao <hosomi.fuminao.df4@jp.nssol.nipponsteel.com>
2020-05-29 13:32:32 +0900

    Update web/app.py for testing CodeDeploy and CodePipeline

--- web/app.py
+++ web/app.py
@@ -10,6 +10,7 @@ def mainmenu():
     <html>
     <body>
     <center><h1>Hello World! from AWS CodeDeploy.</h1><br/>
+    <div>Updated by using CodeDeploy and CodePipeline</div>
     </body>
     </html>"""
 
